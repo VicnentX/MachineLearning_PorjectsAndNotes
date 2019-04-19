1078cf63cffc5c5124de2524260489289a3bbd3b
VicnentX <chunqiu1xia@gmail.com>
2019-04-19 00:15:24 -0700

    change hw1_4

---
+++ ML_homework/dataFrame_matrix_trans.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+
+df = pd.DataFrame(np.random.randn(3, 4), columns=list("abcd"))
+print(df)
+print("_____________")
+print(df.values)
+print("_____________")
+dfT = df.transpose
+
